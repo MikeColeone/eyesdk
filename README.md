b81b907aa448336daae28130d39945dc69d8d16c
MikeColeone <coleonemike@gmail.com>
2025-10-04 23:32:42 +0800

    feat: error

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# WebMonitor
 
+## Introduce
+
+## Use
+
+## Update
+
+## TODO
+
+- **支持按类型分类上报（error、behavior、perf）；**
+- **支持本地存储兜底重试（localStorage + retry 机制）；**
+- **多 tab 通信同步（BroadcastChannel）避免重复；**
+- **上报失败重试（指数退避或最大重试次数）；**
+- **支持队列暂停、恢复、销毁（供插件统一控制）。**
+- 采样率问题
--- src/error/index.ts
+++ src/error/index.ts
@@ -1,5 +1,11 @@
+import { jsLoadError } from "./jsError.js";
+import { promiseError } from "./promiseError.js";
+import { resourceLoadError } from "./resourceError.js";
 export default function error() {
   // 捕获资源加载失败的错误： js css  img
+  resourceLoadError();
   // 捕获js错误
+  jsLoadError();
   // 捕获promise错误  asyn await
+  promiseError();
 }
